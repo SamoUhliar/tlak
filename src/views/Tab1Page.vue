a7fbf9a95f0bab28cb0cb2a8b48df937f552d373
SamoUhliar <samo.uhliar1@gmail.com>
2022-04-05 09:27:59 +0200

    first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,19 +11,37 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-input v-model="sys" type="number" placeholder="SYS"></ion-input>
+      <ion-input v-model="dia" type="number" placeholder="DIA"></ion-input>
+      <ion-input v-model="pulz" type="number" placeholder="PULZ"></ion-input>
+      <ion-datetime v-model="iosTime" style="margin:auto"></ion-datetime>
+      <div style="margin-top:150px;display: flex;justify-content:center;">
+        <ion-button>Pridat</ion-button>
+      </div>  
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonDatetime, IonButton } from '@ionic/vue';
 
-export default  defineComponent({
+export default {
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonDatetime, IonButton },
+  data() 
+  {
+    return {
+      currentTime: undefined,
+      iosTime: '',
+      sys: undefined,
+      dia: undefined,
+      pulz: undefined,
+    }
+  },
+  created() {
+    this.currentTime = new Date(new Date().toLocaleString("en-US", {timeZone: "Europe/Bratislava"})) 
+    this.iosTime = this.currentTime.toISOString()
+    
+  }
+}
 </script>
